76609cadc97d14dbb86cca3c6d665cd9d3036121
Anthony Graniero <ananfro@gmail.com>
2018-10-31 14:16:17 +0000

    automatically backed up by learn

--- lib/shoe.rb
+++ lib/shoe.rb
@@ -13,8 +13,8 @@ class Shoe
     puts "Your shoe is as good as new!"
   end
     
-  def brand=(brand)
-    @brand = brand
+  def brands=(brand)
+    @brands = brand
     BRANDS << brand
   end
 end
